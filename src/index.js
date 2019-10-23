3bf3017dc27393745e5800ab1c16b077ed258c64
Aliaksandr Fiodarau [EPAM] <Aliaksandr_Fiodarau3@epam.com>
2019-10-23 14:17:16 +0300

    Smart calculator

--- src/index.js
+++ src/index.js
@@ -1,27 +1,102 @@
 class SmartCalculator {
-  constructor(initialValue) {
-    // your implementation
-  }
 
-  add(number) {
-    // your implementation
-  }
-  
-  subtract(number) {
-    // your implementation
-  }
+    constructor(initialValue) {
+        this.tmp = [initialValue];
+        this.result = 0;
+    }
 
-  multiply(number) {
-    // your implementation
-  }
+    add(number) {
+        this.tmp.push('+');
+        this.tmp.push(number);
+        this.calculate();
+        return this;
+    }
 
-  devide(number) {
-    // your implementation
-  }
+    subtract(number) {
+        this.tmp.push('-');
+        this.tmp.push(number);
+        this.calculate();
+        return this;
+    }
 
-  pow(number) {
-    // your implementation
-  }
+    multiply(number) {
+        this.tmp.push('*');
+        this.tmp.push(number);
+        this.calculate();
+        return this;
+    }
+
+    devide(number) {
+        this.tmp.push('/');
+        this.tmp.push(number);
+        this.calculate();
+        return this;
+    }
+
+    pow(number) {
+        this.tmp.push('^');
+        this.tmp.push(number);
+        this.calculate();
+        return this;
+    }
+
+    calculate() {
+        let array = [];
+        array = array.concat(this.tmp);
+        if (array.includes("^")) {
+            this.counting(array, "^");
+        }
+        if (array.includes("*")) {
+            this.counting(array, "*");
+        }
+        if (array.includes("/")) {
+            this.counting(array, "/");
+        }
+        if (array.includes("-")) {
+            this.counting(array, "-");
+        }
+        if (array.includes("+")) {
+            this.counting(array, "+");
+        }
+    }
+
+    counting(array, operation) {
+        while (array.includes(operation)) {
+            let index = array.indexOf(operation)
+            let numberOne = Number(array[index - 1]);
+            let numberTwo = Number(array[index + 1]);
+
+            let currentResult;
+
+            switch (operation) {
+                case '/':
+                    currentResult = numberOne / numberTwo;
+                    break;
+                case '*':
+                    currentResult = numberOne * numberTwo;
+                    break;
+                case '-':
+                    currentResult = numberOne - numberTwo;
+                    break;
+                case '+':
+                    currentResult = numberOne + numberTwo;
+                    break;
+                case '^':
+                    index = array.lastIndexOf(operation)
+                    numberOne = Number(array[index - 1]);
+                    numberTwo = Number(array[index + 1]);
+                    currentResult = Math.pow(numberOne, numberTwo);
+                    break;
+            }
+            this.result = currentResult;
+            array.splice(index - 1, 3, currentResult)
+        }
+        return array;
+    }
+
+    toString() {
+        return Number(this.result);
+    }
 }
 
 module.exports = SmartCalculator;
